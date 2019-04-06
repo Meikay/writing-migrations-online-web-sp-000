a0aaca78752d828902d44793cb74f95a8f4d6939
Meisuleen Kased <meisuleen@gmail.com>
2019-04-06 01:36:01 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,9 +1,10 @@
 class  AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   def change
     add_column :students, :grade, :integer
-  end
-
-  def change
     add_column :students, :birthdate, :string
   end
+
+  # def change
+  #   add_column :students, :birthdate, :string
+  # end
 end
